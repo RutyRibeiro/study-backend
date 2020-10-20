7f58d0d2d662559f9fdc76109117d7febd2b923f
RutyRibeiro <64114456+RutyRibeiro@users.noreply.github.com>
2020-10-19 22:32:08 -0300

    alterando função de treinamento

--- Backend/uteis.py
+++ Backend/uteis.py
@@ -68,20 +68,19 @@ def treinaAlgoritmo():
         IDs = []
         for imagePath in imagePaths:
             faceImage = Image.open(imagePath).convert('L')  # Open image and convert to gray
-            faceImage = faceImage.resize((110, 110))  # resize the image so the EIGEN recogniser can be trained
-            faceNP = np.array(faceImage, 'uint8')  # convert the image to Numpy array
-            ID = int(os.path.split(imagePath)[-1].split('.')[1])  # Retreave the ID of the array
-            FaceList.append(faceNP)  # Append the Numpy Array to the list
-            IDs.append(ID)  # Append the ID to the IDs list
-            cv2.imshow('Training Set', faceNP)  # Show the images in the list
-            # cv2.waitKey(1)
-        return np.array(IDs), FaceList  # The IDs are converted in to a Numpy array
-
+            faceImage = faceImage.resize((110,110))         # resize the image so the EIGEN recogniser can be trained
+            faceNP = np.array(faceImage, 'uint8')           # convert the image to Numpy array
+            ID = int(os.path.split(imagePath)[-1].split('.')[1])    # Retreave the ID of the array
+            FaceList.append(faceNP)                         # Append the Numpy Array to the list
+            IDs.append(ID)                                  # Append the ID to the IDs list
+            cv2.imshow('Training Set', faceNP)              # Show the images in the list
+            cv2.waitKey(1)
+        return np.array(IDs), FaceList                      # The IDs are converted in to a Numpy array
     IDs, FaceList = getImageWithID(path)
 
     # ------------------------------------ TRAING THE RECOGNISER ----------------------------------------
     print('TRAINING......')
-    EigenFace.train(FaceList, IDs)  # The recongniser is trained using the images
+    EigenFace.train(FaceList, IDs)                          # The recongniser is trained using the images
     print('EIGEN FACE RECOGNISER COMPLETE...')
     EigenFace.save('Recogniser/trainingDataEigan.xml')
     print('FILE SAVED..')
@@ -92,9 +91,9 @@ def treinaAlgoritmo():
     LBPHFace.train(FaceList, IDs)
     print('LBPH FACE RECOGNISER COMPLETE...')
     LBPHFace.save('Recogniser/trainingDataLBPH.xml')
-    print('ALL XML FILES SAVED...')
+    print ('ALL XML FILES SAVED...')
 
-    # cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
 
 def reconheceFoto(img):
     face_cascade = cv2.CascadeClassifier('Haar/haarcascade_frontalcatface.xml')
