d2a02ef68ef2f37dfeb0e7a7782027fbf2b7fde7
RutyRibeiro <64114456+RutyRibeiro@users.noreply.github.com>
2020-11-11 19:28:40 -0300

    Capitalize nome

--- Backend/flask_modulos.py
+++ Backend/flask_modulos.py
@@ -27,7 +27,7 @@ def verifica (request):
 
 def cadastro(body):
     response={}
-    response = reco_Modulos.cadastra(body['video'], body['nome'])
+    response = reco_Modulos.cadastra(body['video'], (body['nome'].capitalize))
     return response
 
 def login(img):
